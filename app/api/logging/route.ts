61b11ef3f9fe3f0b68c73af0c175d9a0ae22dcd8
Ruben Verster <rrverster31415@gmail.com>
2025-02-07 17:33:19 +0200

    cleanup

--- app/api/logging/route.ts
+++ app/api/logging/route.ts
@@ -8,7 +8,6 @@ export async function POST(request: NextRequest) {
   try {
     const { visitorId, action } = await request.json();
 
-    console.log(`TEST`, visitorId, action);
     if (!visitorId || !action) {
       return NextResponse.json(
         { error: "Missing visitorId or action" },
